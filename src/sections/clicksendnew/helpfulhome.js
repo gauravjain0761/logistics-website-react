546f6c592845d6d5721631532e8b7f4b04804e6c
bilalmansuri <bilalmansuri350@gmail.com>
2023-07-14 14:26:01 +0530

    fixed

--- src/sections/clicksendnew/helpfulhome.js
+++ src/sections/clicksendnew/helpfulhome.js
@@ -7,128 +7,176 @@ const HelpfulHome = () => {
     <React.Fragment>
       <Box py={4} sx={{ backgroundColor: "#000000" }}>
         <Container>
-          <Grid container spacing={1} borderBottom="1px solid grey" py={4}>
-            <Grid item lg={6}>
-              <CardContent>
-                <Box component="img" src="/box.png" alt="truck" />
-              </CardContent>
-            </Grid>
-            <Grid item lg={6}>
-              <Grid container spacing={4}>
-                <Grid item sm={6} md={6} lg={6}>
-                  <Box>
-                    <Iconify
-                      sx={{ color: "#ff7534" }}
-                      icon="akar-icons:info-fill"
-                      width="35px"
-                    />
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" color="#fff">
-                      Help Center
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <Typography color="grey">
-                      Customer Service Customer Portal Logins
-                    </Typography>
-                  </Box>
-                </Grid>
-                <Grid item sm={6} md={6} lg={6}>
-                  <Box>
-                    <Iconify
-                      sx={{ color: "#ff7534" }}
-                      icon="bxs:user"
-                      width="35px"
-                    />
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" color="#fff">
-                      Business
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <Typography color="grey">
-                      New Customer Center Service Guide
-                    </Typography>
-                  </Box>
-                </Grid>
-                <Grid item sm={6} md={6} lg={6}>
-                  <Box>
-                    <Iconify
-                      sx={{ color: "#ff7534" }}
-                      icon="mingcute:ship-fill"
-                      width="35px"
-                    />
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" color="#fff">
-                      Ship Online Now
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <Typography color="grey">
-                      Open a Account Ship Managre Software Tracking
-                    </Typography>
-                  </Box>
-                </Grid>
-                <Grid item sm={6} md={6} lg={6}>
-                  <Box>
-                    <Iconify
-                      sx={{ color: "#ff7534" }}
-                      icon="fa6-solid:building"
-                      width="35px"
-                    />
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" color="#fff">
-                      Company
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <Typography color="grey">About Us Careers</Typography>
-                  </Box>
-                </Grid>
-                <Grid item sm={6} md={6} lg={6}>
-                  <Box>
-                    <Iconify
-                      sx={{ color: "#ff7534" }}
-                      icon="bxs:news"
-                      width="35px"
-                      hFlip={true}
-                    />
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" color="#fff">
-                      News
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <Typography color="grey">
-                      Service News for Customers Events
-                    </Typography>
-                  </Box>
-                </Grid>
-                <Grid item sm={6} md={6} lg={6}>
-                  <Box>
-                    <Iconify
-                      sx={{ color: "#ff7534" }}
-                      icon="la:blog"
-                      width="35px"
-                    />
-                  </Box>
-                  <Box>
-                    <Typography variant="h6" color="#fff">
-                      Our Blog
-                    </Typography>
-                  </Box>
-                  <Box>
-                    <Typography color="grey">Our Blog for customers</Typography>
-                  </Box>
+          <Container>
+            <Grid container spacing={8} borderBottom="1px solid grey" py={4}>
+              <Grid item lg={6}>
+                <CardContent>
+                  <Box component="img" src="/box.png" alt="truck" />
+                </CardContent>
+              </Grid>
+              <Grid item lg={6}>
+                <Grid container rowSpacing={6} columnSpacing={10}>
+                  <Grid item sm={6} md={6} lg={6}>
+                    <Box>
+                      <Iconify
+                        sx={{ color: "#ff7534" }}
+                        icon="akar-icons:info-fill"
+                        width="35px"
+                      />
+                    </Box>
+                    <Box>
+                      <Typography
+                        variant="h6"
+                        sx={{ fontSize: "16px !important" }}
+                        color="#fff"
+                      >
+                        Help Center
+                      </Typography>
+                    </Box>
+                    <Box>
+                      <Typography
+                        color="grey"
+                        sx={{ fontSize: "14px !important" }}
+                      >
+                        Customer Service Customer Portal Logins
+                      </Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item sm={6} md={6} lg={6}>
+                    <Box>
+                      <Iconify
+                        sx={{ color: "#ff7534" }}
+                        icon="bxs:user"
+                        width="35px"
+                      />
+                    </Box>
+                    <Box>
+                      <Typography
+                        variant="h6"
+                        sx={{ fontSize: "16px !important" }}
+                        color="#fff"
+                      >
+                        Business
+                      </Typography>
+                    </Box>
+                    <Box>
+                      <Typography
+                        color="grey"
+                        sx={{ fontSize: "14px !important" }}
+                      >
+                        New Customer Center Service Guide
+                      </Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item sm={6} md={6} lg={6}>
+                    <Box>
+                      <Iconify
+                        sx={{ color: "#ff7534" }}
+                        icon="mingcute:ship-fill"
+                        width="35px"
+                      />
+                    </Box>
+                    <Box>
+                      <Typography
+                        variant="h6"
+                        sx={{ fontSize: "16px !important" }}
+                        color="#fff"
+                      >
+                        Ship Online Now
+                      </Typography>
+                    </Box>
+                    <Box>
+                      <Typography
+                        color="grey"
+                        sx={{ fontSize: "14px !important" }}
+                      >
+                        Open a Account Ship Managre Software Tracking
+                      </Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item sm={6} md={6} lg={6}>
+                    <Box>
+                      <Iconify
+                        sx={{ color: "#ff7534" }}
+                        icon="fa6-solid:building"
+                        width="35px"
+                      />
+                    </Box>
+                    <Box>
+                      <Typography
+                        variant="h6"
+                        sx={{ fontSize: "16px !important" }}
+                        color="#fff"
+                      >
+                        Company
+                      </Typography>
+                    </Box>
+                    <Box>
+                      <Typography
+                        color="grey"
+                        sx={{ fontSize: "14px !important" }}
+                      >
+                        About Us Careers
+                      </Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item sm={6} md={6} lg={6}>
+                    <Box>
+                      <Iconify
+                        sx={{ color: "#ff7534" }}
+                        icon="bxs:news"
+                        width="35px"
+                        hFlip={true}
+                      />
+                    </Box>
+                    <Box>
+                      <Typography
+                        variant="h6"
+                        sx={{ fontSize: "16px !important" }}
+                        color="#fff"
+                      >
+                        News
+                      </Typography>
+                    </Box>
+                    <Box>
+                      <Typography
+                        color="grey"
+                        sx={{ fontSize: "14px !important" }}
+                      >
+                        Service News for Customers Events
+                      </Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item sm={6} md={6} lg={6}>
+                    <Box>
+                      <Iconify
+                        sx={{ color: "#ff7534" }}
+                        icon="la:blog"
+                        width="35px"
+                      />
+                    </Box>
+                    <Box>
+                      <Typography
+                        variant="h6"
+                        sx={{ fontSize: "16px !important" }}
+                        color="#fff"
+                      >
+                        Our Blog
+                      </Typography>
+                    </Box>
+                    <Box>
+                      <Typography
+                        color="grey"
+                        sx={{ fontSize: "14px !important" }}
+                      >
+                        Our Blog for customers
+                      </Typography>
+                    </Box>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-          </Grid>
+          </Container>
         </Container>
       </Box>
     </React.Fragment>
--- src/sections/clicksendnew/ourfleet.js
+++ src/sections/clicksendnew/ourfleet.js
@@ -1,17 +1,37 @@
 import Iconify from "@/components/iconify/Iconify";
-import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Grid,
+  IconButton,
+  Stack,
+  Typography,
+  alpha,
+} from "@mui/material";
 import React from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const OurFleet = () => {
   return (
     <React.Fragment>
-      <Box py={4}>
+      <Box py={8}>
         <Container>
-          <Grid container spacing={3}>
+          <Grid container columnSpacing={3}>
             <Grid item lg={4}>
               <Box>
-                <Typography variant="h3">Our Fleet</Typography>
-                <Typography color="grey">
+                <Typography
+                  variant="h3"
+                  sx={{ fontSize: "28px !important", mb: 2, fontWeight: 700 }}
+                >
+                  Our Fleet
+                </Typography>
+                <Typography
+                  color="grey"
+                  sx={{ fontSize: "14px", lineHeight: "1.5rem" }}
+                >
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry{"'"}s
                   standard dummy text ever sinceLorem Ipsum is simply dummy text
@@ -19,7 +39,7 @@ const OurFleet = () => {
                 </Typography>
               </Box>
             </Grid>
-            <Grid item lg={2.60}>
+            <Grid item lg={2.6}>
               <Stack justifyContent="center" direction="column">
                 <Box
                   component="img"
@@ -31,14 +51,55 @@ const OurFleet = () => {
                   Extensive equipment and consolidation options
                 </Typography>
                 <Box>
-                <Button endIcon={<Iconify icon="ep:right" color="primary"  />}>View All</Button>
+                  <Button endIcon={<Iconify icon="ep:right" color="primary" />}>
+                    View All
+                  </Button>
+                </Box>
+                <Box sx={{ mt: 4 }}>
+                  <Stack direction="row" spacing={2}>
+                    <Card
+                      variant="outlined"
+                      sx={{
+                        borderRadius: "50%",
+                        borderColor: (theme) =>
+                          alpha(theme.palette.grey[900], 0.4),
+                      }}
+                    >
+                      <IconButton>
+                        <ArrowBackIcon
+                          sx={{
+                            color: (theme) =>
+                              alpha(theme.palette.common.black, 0.9),
+                            fontWeight: 700,
+                          }}
+                        />
+                      </IconButton>
+                    </Card>
+                    <Card
+                      variant="outlined"
+                      sx={{
+                        borderRadius: "50%",
+                        borderColor: (theme) =>
+                          alpha(theme.palette.grey[900], 0.4),
+                      }}
+                    >
+                      <IconButton>
+                        <ArrowForwardIcon
+                          sx={{
+                            color: (theme) =>
+                              alpha(theme.palette.common.black, 0.9),
+                            fontWeight: 700,
+                          }}
+                        />
+                      </IconButton>
+                    </Card>
+                  </Stack>
                 </Box>
               </Stack>
             </Grid>
-            <Grid item lg={2.60}>
+            <Grid item lg={2.6}>
               <Stack justifyContent="center" direction="column">
                 <Box
-                
                   component="img"
                   src="/assets/images/home/refrigrato.png"
                   alt="truck"
@@ -48,11 +109,13 @@ const OurFleet = () => {
                   Extensive equipment and consolidation options
                 </Typography>
                 <Box>
-                <Button endIcon={<Iconify icon="ep:right" color="primary"  />}>View All</Button>
+                  <Button endIcon={<Iconify icon="ep:right" color="primary" />}>
+                    View All
+                  </Button>
                 </Box>
               </Stack>
             </Grid>
-            <Grid item lg={2.60}>
+            <Grid item lg={2.6}>
               <Stack justifyContent="center" direction="column">
                 <Box
                   component="img"
@@ -64,7 +127,9 @@ const OurFleet = () => {
                   Extensive equipment and consolidation options
                 </Typography>
                 <Box>
-                <Button endIcon={<Iconify icon="ep:right" color="primary"  />}>View All</Button>
+                  <Button endIcon={<Iconify icon="ep:right" color="primary" />}>
+                    View All
+                  </Button>
                 </Box>
               </Stack>
             </Grid>
