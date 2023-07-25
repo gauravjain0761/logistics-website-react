efe5515af8e1327ab3d2ebd5f5a8abffa2caafb8
Shoaib <Shoaibk909@gmail.com>
2023-07-25 12:17:40 +0530

    Home-animation-done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,14 +15,14 @@ export default function App(props) {
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <React.Fragment>
-      <MotionLazyContainer>
+      {/* <MotionLazyContainer> */}
         <ThemeProvider>
           <ProgressBar />
           <SnackbarProvider>
             {getLayout(<Component {...pageProps} />)}
           </SnackbarProvider>
         </ThemeProvider>
-      </MotionLazyContainer>
+      {/* </MotionLazyContainer> */}
     </React.Fragment>
   );
 }
