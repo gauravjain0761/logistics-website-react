213f2865367d160bdada61621ca18bb95f438243
bilalmansuri <bilalmansuri350@gmail.com>
2023-08-25 17:03:59 +0530

    fixed

---
+++ src/components/button/submitButton.js
@@ -0,0 +1,22 @@
+import { Button } from "@mui/material";
+import MuiCircularProgress from "@mui/material/CircularProgress";
+import { styled } from "@mui/material/styles";
+import PropTypes from "prop-types";
+
+const CircularProgress = styled(MuiCircularProgress)(() => ({
+  marginLeft: 10
+}));
+
+const SubmitButton = ({ title, loading, variant, disabled = false }) =>
+  <Button disabled={disabled} variant={variant} type="submit">
+    {title}
+    {loading && <CircularProgress color="inherit" size={20} />}
+  </Button>;
+
+SubmitButton.propTypes = {
+  title: PropTypes.string,
+  loading: PropTypes.bool,
+  variant: PropTypes.string
+};
+
+export default SubmitButton;
