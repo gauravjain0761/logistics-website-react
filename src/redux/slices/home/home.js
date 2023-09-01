4f9b6e7eba7afd42898b33336d67a744533b4e76
bilalmansuri <bilalmansuri350@gmail.com>
2023-09-01 16:20:00 +0530

    fixed

--- src/redux/slices/home/home.js
+++ src/redux/slices/home/home.js
@@ -9,18 +9,32 @@ const initialState = {
   totalUser: {
     data: [],
   },
+  isTestimonialLoading: false,
+  testimonialError: null,
+  testimonial: {
+    data: [],
+  },
 };
 const slice = createSlice({
   name: "home",
   initialState,
   reducers: {
     // START LOADING
+
     startTotalUserLoading(state) {
+      state.isTotalUserLoading = true;
+    },
+    startTestimonialLoading(state) {
       state.isTotalUserLoading = true;
     },
 
     // HAS ERROR START
+
     hasTotalUserError(state, action) {
+      state.isTotalUserLoading = false;
+      state.error = action.payload;
+    },
+    hasTestimonialError(state, action) {
       state.isTotalUserLoading = false;
       state.error = action.payload;
     },
@@ -28,12 +42,22 @@ const slice = createSlice({
     // HAS ERROR END
 
     // SET TOTAL USER
+
     setTotalUser(state, action) {
       state.isTotalUserLoading = false;
       state.totalUser.data = action.payload;
     },
 
     // SET TOTAL USER END
+
+    // SET TESTIMONIAL
+
+    setTestimonial(state, action) {
+      state.isTestimonialLoading = false;
+      state.testimonial.data = action.payload;
+    },
+
+    // SET TESTIMONIAL END
 
     // =====================================================
   },
@@ -43,7 +67,7 @@ const slice = createSlice({
 export default slice.reducer;
 
 // Actions
-export const { startTotalUserLoading } = slice.actions;
+export const { startTotalUserLoading, startTestimonialLoading } = slice.actions;
 
 // ----------------------------------------------------------------------
 
@@ -60,3 +84,15 @@ export const getTotalUser = () => {
 };
 
 // ==========================================================
+
+export const getTestimonial = () => {
+  return async (dispatch) => {
+    dispatch(slice.actions.startTestimonialLoading());
+    try {
+      const response = await axiosInstance.get("api/home/faq-testimonial");
+      dispatch(slice.actions.setTestimonial(response?.data?.view_data));
+    } catch (error) {
+      dispatch(slice.actions.hasTestimonialError(error));
+    }
+  };
+};
